a465b74075a1e865f4ad7322000e3165867cc928
Teintro <127615450+Teintro@users.noreply.github.com>
2023-08-12 13:14:10 -0500

    Add files via upload

---
+++ si/hola.js
@@ -0,0 +1,11 @@
+let mario = document.getElementsByClassName("mario")
+console.log(mario)
+
+let luigi = document.querySelectorAll("luigi")
+console.log(luigi)
+
+let car = document.getElementById("car")
+console.log(car)
+
+let pls = document.querySelector("pls")
+console.log(pls)
